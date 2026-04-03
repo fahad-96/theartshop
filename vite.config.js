3c59f09a17e6fca742a392d65850a08cf51b57df
artshop <fahadyousuf101@gmail.com>
2026-04-03 17:57:20 +0530

    Add Netlify build config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/theartshop/',
+export default defineConfig(() => ({
+  base: process.env.NETLIFY ? '/' : '/theartshop/',
   plugins: [react()],
-})
+}))
